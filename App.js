85686eb4746b50d659087c56b75b495b1e497563
Primidac <primidac@gmail.com>
2024-01-02 21:39:23 +0100

    Performing an overhaul

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import React from 'react';
-import MyStack from './routes/MyStack';
-import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
-
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const scheme = useColorScheme();
-  const MyDarkTheme = {
-    dark: true,
-    colors: {
-      primary: '#00a78e',
-      background: '#1b1c1e',
-      text: '#ffffff'
-    }
-  }
-  const MyLightTheme = {
-    dark: false,
-    colors: {
-      primary: '#153f38',
-      background: '#fff',
-      text: '#153f38'
-    }
-  }
-
   return (
-    <AppearanceProvider>
-      <NavigationContainer theme={scheme === 'dark' ? MyDarkTheme: MyLightTheme  }>
-        <MyStack />
-      </NavigationContainer>
-    </AppearanceProvider>
-
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- README.md
+++
@@ -1,2 +0,0 @@
-# safebrik
-Sleek password and phrase manager for the 21st century
